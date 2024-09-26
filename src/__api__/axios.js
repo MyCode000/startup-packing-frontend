0bdffc79ea6fff4cbb8370cffe5964025f92145a
Yossef Aita <yossefaita176@gmail.com>
2024-09-26 10:37:52 +0300

    change base url

--- src/__api__/axios.js
+++ src/__api__/axios.js
@@ -1,8 +1,8 @@
 "use client";
 import axios from "axios";
 
-// https://wishlist-backend-production-9dc0.up.railway.app  // http://127.0.0.1:8000
-export const mainUrl = "http://127.0.0.1:8000";
+// https://startup-packing-c4817.up.railway.app  // http://127.0.0.1:8000
+export const mainUrl = "https://startup-packing-c4817.up.railway.app";
 
 // Function to create axios instance with necessary headers
 const createAxiosInstance = () => {
